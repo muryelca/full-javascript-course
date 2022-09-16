c4495684d42f3d411e942d48cf8a1667faae9ba1
Muryel cabral <muryel_msn@hotmail.com>
2022-09-16 09:36:49 -0300

    feat: added basic and avanced functions

---
+++ 3-funcoes-avançadas/call_apply.js
@@ -0,0 +1,15 @@
+function teste(str, n) {
+    console.log(this.name)
+    console.log(str, n)
+}
+
+teste.call({ name: "Maria" }, "string", 20)
+teste.apply({ name: "João" }, [" da Silva", 28])
+teste.call({ name: "João" }, ...[" da Silva", 28])
+
+const teste2 = teste.bind({ name: "Joana" })
+
+teste2("Joaquina", 30)
+teste("Manuela", 40)
+
+document.addEventListener("click", teste2)
